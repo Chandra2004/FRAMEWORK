c10af9438ef62624033f8743b0650ff45fee4191
Chandra2004 <chandratriantomo123@gmail.com>
2026-01-24 20:37:09 +0700

    debug: Enable error display to identify 500 error cause

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require_once __DIR__ . '/app/Helpers/helpers.php';
 require_once __DIR__ . '/vendor/autoload.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('ROOT_DIR', __DIR__);
 
 // Load Routes
